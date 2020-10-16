72b8f7dc123f09fecfd372c6a0b739937059b60f
Administratorius <Administratorius@DESKTOP-NCUDGGO>
2020-10-16 17:28:53 +0300

    2020-10-16 17:30

--- 2020-10-16/src/lt/vtmc/examone/BankImplementation.java
+++ 2020-10-16/src/lt/vtmc/examone/BankImplementation.java
@@ -7,60 +7,81 @@ import java.util.List;
 
 public class BankImplementation implements Bank {
 
-    private final CurrencyConverter currencyConverter;
-    private final List<Customer> customers;
-    private SequenceGenerator customerSequence = new SequenceGenerator();
-    private SequenceGenerator accountSequence = new SequenceGenerator();
+	private final CurrencyConverter currencyConverter;
+	private final List<Customer> customers;
+	private SequenceGenerator customerSequence = new SequenceGenerator();
+	private SequenceGenerator accountSequence = new SequenceGenerator();
+	private SequenceGenerator operationSequence = new SequenceGenerator();
 
-    public BankImplementation(CurrencyConverter currencyConverter) {
-        this.currencyConverter = currencyConverter;
-        this.customers = new ArrayList<>();
-    }
+	public BankImplementation(CurrencyConverter currencyConverter) {
+		this.currencyConverter = currencyConverter;
+		this.customers = new ArrayList<>();
+	}
 
-    @Override
-    public Customer createCustomer(PersonCode personCode, PersonName personName) {
-        if (personCode == null || personName == null){
-            throw new NullPointerException();
-        }
+	@Override
+	public Customer createCustomer(PersonCode personCode, PersonName personName) {
+		if (personCode == null || personName == null) {
+			throw new NullPointerException();
+		}
 
-        for (Customer customer : customers) {
-            if (customer.getPersonCode().equals(personCode)){
-                throw new CustomerCreateException("Customer already exists ...");
-            }
-        }
+		for (Customer customer : customers) {
+			if (customer.getPersonCode().equals(personCode)) {
+				throw new CustomerCreateException("Customer already exists ...");
+			}
+		}
 
-        Customer customer = new Customer(customerSequence.getNext(), personCode, personName);
-        customers.add(customer);
+		Customer customer = new Customer(customerSequence.getNext(), personCode, personName);
+		customers.add(customer);
 
-        return customer;
-    }
+		return customer;
+	}
 
-    @Override
-    public Account createAccount(Customer customer, Currency currency) {
-        if (customer == null || currency == null){
-            throw new NullPointerException();
-        }
-        if (!customers.contains(customer)){
-            throw new CustomerCreateException("Customer does not exist ...");
-        }
+	@Override
+	public Account createAccount(Customer customer, Currency currency) {
+		if (customer == null || currency == null) {
+			throw new NullPointerException();
+		}
+		if (!customers.contains(customer)) {
+			throw new AccountCreateException("Customer does not exist ...");
+		}
 
-        Account account = new Account(accountSequence.getNext(), customer, currency, new Money(0.0d));
-        customer.addAccount(account);
-        return account;
-    }
+		Account account = new Account(accountSequence.getNext(), customer, currency, new Money(0.0d));
+		customer.addAccount(account);
+		return account;
+	}
 
-    @Override
-    public Operation transferMoney(Account debitAccount, Account creditAccount, Money debitAmount) {
-        if (debitAccount.getBalance().isLessThan(debitAmount)){
-            throw new InsufficientFundsException("You are bad customer!");
-        }
-        debitAccount.setBalance(debitAccount.getBalance().substract(debitAmount));
-        // TODO
-        return null;
-    }
+	@Override
+	public Operation transferMoney(Account debitAccount, Account creditAccount, Money debitAmount) {
+		// Check account balance
+		if (debitAccount.getBalance().isLessThan(debitAmount)) {
+			throw new InsufficientFundsException("You are bad customer!");
+		}
+		// debitAccount - debitAmount
+		debitAccount.setBalance(debitAccount.getBalance().substract(debitAmount));
+		// Convert money from debitAmount currency to creditAccount currency
+		final Money creditAmount = currencyConverter.convert(debitAccount.getCurrency(), creditAccount.getCurrency(),
+				debitAmount);
+		// debitAmount transfer to creditAccount
+		creditAccount.setBalance(creditAccount.getBalance().add(creditAmount));
+		// Return Operation object with operation details
+		return new Operation(operationSequence.getNext(), debitAccount, creditAccount, debitAmount);
+	}
 
-    @Override
-    public Money getBalance(Currency currency) {
-        return null;
-    }
+	@Override
+	public Money getBalance(Currency currency) {
+		// Create balance object for customer money
+		Money balance = new Money(0.0d);
+		// Iterate by customers list
+		for (Customer customer : customers) {
+			// Get all accounts from customer
+			for (Account account : customer.getAccounts()) {
+				if(account.getCurrency().equals(currency)) {
+					balance = balance.add(account.getBalance());
+				} else {
+					balance = balance.add(currencyConverter.convert(account.getCurrency(), currency, account.getBalance()));
+				}
+			}
+		}
+		return balance;
+	}
 }
--- 2020-10-16/src/lt/vtmc/examone/CurrencyConverterImplementation.java
+++ 2020-10-16/src/lt/vtmc/examone/CurrencyConverterImplementation.java
@@ -1,17 +1,25 @@
 package lt.vtmc.examone;
 
 import lt.itakademija.exam.Currency;
+import lt.itakademija.exam.CurrencyConversionException;
 import lt.itakademija.exam.CurrencyConverter;
 import lt.itakademija.exam.CurrencyRatesProvider;
 import lt.itakademija.exam.Money;
 
 public class CurrencyConverterImplementation implements CurrencyConverter {
 
-    public CurrencyConverterImplementation(CurrencyRatesProvider currencyRatesProvider) {
+    private final CurrencyRatesProvider currencyRatesProvider;
+
+	public CurrencyConverterImplementation(CurrencyRatesProvider currencyRatesProvider) {
+    	this.currencyRatesProvider = currencyRatesProvider;
     }
 
     @Override
-    public Money convert(Currency currency, Currency currency1, Money money) {
-        return null;
+    public Money convert(Currency from, Currency to, Money amount) {
+    	Money rate = currencyRatesProvider.getRate(from, to);
+    	if (rate == null) {
+    		throw new CurrencyConversionException("Could not convert currency ...");
+    	}
+        return amount.multiply(rate);
     }
 }
